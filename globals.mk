b36b412632b4a9960c45677879e9158ec64990db
nick <goodfella005@gmail.com>
2009-04-26 15:28:02 -0700

    renamed globals.mk to variables.mk

--- globals.mk
+++
@@ -1,7 +0,0 @@
-# global variables used by the build system
-
-# suffix for C++ shared library object files
-cxx_shlib_obj := cxx.shlib.o
-
-# suffix for C++ program object files
-cxx_prog_obj := cxx.o
