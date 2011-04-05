ccde33f85e64a07bc75b988706fbfc16e9e8a2ca
nick <goodfella005@gmail.com>
2011-04-05 00:18:34 -0700

    Remove duplicate cppflags:
    * Adding the filtered cppflags from cflags was causing the cppflags to
      show up twice in the command line.

--- modules.mk
+++ modules.mk
@@ -237,7 +237,7 @@ define create_target_vars
 $(2): TARGET_CFLAGS := $(call cflags,$1)
 $(2): PREREQ_INCDIRS := $(call prepend_gcc_incdirs,$(call src_incdirs,$1))
 $(2): PREREQ_CFLAGS := $(call src_cflags,$1)
-$(2): PREREQ_CPPFLAGS := $(call filter_gcc_cppflags,$(call src_cflags,$1)) $(call prepend_gcc_cppflags,$(call src_cppflags,$1))
+$(2): PREREQ_CPPFLAGS := $(call prepend_gcc_cppflags,$(call src_cppflags,$1))
 $(2): TARGET_LIBDIRS := $(call prepend_gcc_libdirs,$(call libdirs,$1))
 $(2): TARGET_LIBS := $(call prepend_gcc_link_libs,$(call libs,$1))
 $(2): TARGET_PRELIBS := $(call prelibs,$1)
@@ -402,6 +402,6 @@ define src_vars
 # create a target specific variable for each source attribute
 $(2): SRC_VAR_CFLAGS := $(call cflags,$1)
 $(2): SRC_VAR_INCDIRS := $(call prepend_gcc_incdirs,$(call incdirs,$1))
-$(2): SRC_VAR_CPPFLAGS := $(call filter_gcc_cppflags,$(call cflags,$1)) (call prepend_gcc_cppflags,$(call cppflags,$1))
+$(2): SRC_VAR_CPPFLAGS := $(call prepend_gcc_cppflags,$(call cppflags,$1))
 $(call reset_attributes,$1)
 endef
